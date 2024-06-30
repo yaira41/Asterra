6526edd4d1d825cd4430a34e985b09e894ab68ba
Yair Avivi <yavivi441@gmail.com>
2024-06-30 17:01:05 +0300

    get all hobbies

--- server/src/models/hobbies.ts
+++ server/src/models/hobbies.ts
@@ -1,5 +1,11 @@
+import pool from "../config/db";
 
 type Hobby = {
   user_id: number;
   hobbies: string[];
+};
+
+const getHobbies = async (): Promise<Hobby[]> => {
+  const result = await pool.query("SELECT * FROM hobbies");
+  return result.rows;
 };
