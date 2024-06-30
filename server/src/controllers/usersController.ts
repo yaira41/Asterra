b0246178b7bedb5cbd8509775a95bdbfa96a02a8
Yair Avivi <yavivi441@gmail.com>
2024-06-30 17:09:56 +0300

    add getSingleUser function

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -14,3 +14,20 @@ const getAllUsers = async (req: Request, res: Response) => {
   }
 };
 
+const getSingleUser = async (req: Request, res: Response) => {
+  try {
+    const user = await getUserById(Number(req.params.id));
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: "An unknown error occurred" });
+    }
+  }
+};
+
