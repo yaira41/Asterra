80959e0b21aae0b2d546a16ff779180c62838ff2
Yair Avivi <yavivi441@gmail.com>
2024-06-30 17:10:20 +0300

    add addUser function

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -31,3 +31,16 @@ const getSingleUser = async (req: Request, res: Response) => {
   }
 };
 
+const addUser = async (req: Request, res: Response) => {
+  try {
+    const user = await createUser(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: "An unknown error occurred" });
+    }
+  }
+};
+
