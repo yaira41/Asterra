3985cd2ba1195928cef41e1e5223683ca6972c4b
Yair Avivi <yavivi441@gmail.com>
2024-06-30 17:13:08 +0300

    add getAllHobbies function

---
+++ server/src/controllers/hobbiesController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import {
+  getHobbies,
+  getHobbiesByUserId,
+  createHobby,
+  deleteHobby,
+} from "../models/hobbies";
+
+const getAllHobbies = async (req: Request, res: Response) => {
+  try {
+    const hobbies = await getHobbies();
+    res.status(200).json(hobbies);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: "An unknown error occurred" });
+    }
+  }
+};
+
