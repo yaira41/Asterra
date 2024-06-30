be6447b3e74d90f5f2019b69ac185fa4219ad7bf
Yair Avivi <yavivi441@gmail.com>
2024-06-30 17:10:50 +0300

    add removeUser and export all functions

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -44,3 +44,21 @@ const addUser = async (req: Request, res: Response) => {
   }
 };
 
+const removeUser = async (req: Request, res: Response) => {
+  try {
+    const user = await deleteUser(Number(req.params.id));
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: "An unknown error occurred" });
+    }
+  }
+};
+
+export { getAllUsers, getSingleUser, addUser, removeUser };
