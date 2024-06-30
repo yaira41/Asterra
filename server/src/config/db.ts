8957afb6914ff45e912ab218ca4d74352c6e6089
Yair Avivi <yavivi441@gmail.com>
2024-06-30 20:02:40 +0300

    add db configuration

---
+++ server/src/config/db.ts
@@ -0,0 +1,13 @@
+
+const pool = new Pool({
+  user: "postgres",
+  host: "taskdb.cv0wo6aqww1c.us-east-2.rds.amazonaws.com",
+  database: "taskdb",
+  password: "bMk26FPvmyH8VxU",
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export default pool;
