b31dd27efa63be3c3421004a9f8bc9740c486fde
Yair Avivi <yavivi441@gmail.com>
2024-06-30 17:17:58 +0300

    use the routes on the server

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
+import hobbiesRoutes from "./src/routes/hobbiesRoutes";
+import usersRoutes from "./src/routes/usersRoutes";
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use("/users", usersRoutes);
+app.use("/hobbies", hobbiesRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
